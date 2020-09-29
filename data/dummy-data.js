b5b81126c6ef1b50fe84a8e0918faf174d6a3381
JM De Ocampo <jmdoescode@gmail.com>
2020-09-29 00:30:40 -0700

    add dummy-data

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,11 +1,14 @@
-new Product(
-  'p1',
-  'u1',
-  'Red Shirt',
-  'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-  'A red t-shirt, perfect for days with non-red weather.',
-  29.99
-),
+import Product from '../models/product';
+
+const PRODUCTS = [
+  new Product(
+    'p1',
+    'u1',
+    'Red Shirt',
+    'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    'A red t-shirt, perfect for days with non-red weather.',
+    29.99
+  ),
   new Product(
     'p2',
     'u1',
@@ -47,3 +50,5 @@ new Product(
     5.49
   )
 ];
+
+export default PRODUCTS;

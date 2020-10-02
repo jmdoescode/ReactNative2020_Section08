17a1758baa79ed47bd5c558e862c85102b5142a1
JM De Ocampo <jmdoescode@gmail.com>
2020-10-01 22:41:17 -0700

    8.168 - Using custom Fonts

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,23 +1,29 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import { Platform } from 'react-native';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
+import {Platform} from 'react-native';
 import ProductOverviewScreen from "../screens/shop/ProductOverviewScreen";
 import Colors from '../constants/Colors';
 import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 
 const ProductsNavigator = createStackNavigator(
-    {
-        ProductsOverview: ProductOverviewScreen,
-        ProductDetail: ProductDetailScreen
-    },
-    {
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
-            },
-            headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
-        }
+  {
+    ProductsOverview: ProductOverviewScreen,
+    ProductDetail: ProductDetailScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primary : ''
+      },
+      headerTitleStyle: {
+        fontFamily: "open-sans-bold"
+      },
+      headerBackTitleStyle: {
+        fontFamily: "open-sans"
+      },
+      headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary
     }
+  }
 );
 
 export default createAppContainer(ProductsNavigator);

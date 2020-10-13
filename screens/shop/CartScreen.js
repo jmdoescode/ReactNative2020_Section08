1dd13c0cbff112a225c92d795f7e1bf59a00b4de
JM De Ocampo <jmdoescode@gmail.com>
2020-10-13 00:38:43 -0700

    Add Logic to Delete Items

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Button, StyleSheet, Text, View, FlatList} from "react-native";
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import Colors from "../../constants/Colors";
 import CartItem from "../../components/shop/CartItem";
+import * as cartActions from './../../store/actions/cart';
 
 const CartScreen = props => {
   const cartTotalAmount = useSelector(state => state.cart.totalAmount);
@@ -17,8 +18,10 @@ const CartScreen = props => {
         sum: state.cart.items[key].sum
       });
     }
-    return transformedCartItems;
+    return transformedCartItems.sort((a, b) => a.productId > b.productId ? 1 : -1);
   });
+
+  const dispatch = useDispatch();
 
   return (
     <View style={styles.screen}>
@@ -40,7 +43,7 @@ const CartScreen = props => {
             quantity={itemData.item.quantity}
             title={itemData.item.productTitle}
             amount={itemData.item.sum}
-            onRemove={() => {}}
+            onRemove={() => {dispatch(cartActions.removeFromCart(itemData.item.productId))}}
           />
         }
       />
--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,4 +1,4 @@
-import {ADD_TO_CART} from "../actions/cart";
+import {ADD_TO_CART, REMOVE_FROM_CART} from "../actions/cart";
 import CartItem from "../../models/cart-item";
 
 const initialState = {
@@ -22,18 +22,43 @@ export default (state = initialState, action) => {
           prodTitle,
           state.items[addedProduct.id].sum + prodPrice
         );
-        console.log("existing");
       } else {
         newOrExistingItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
-        console.log("new");
       }
-
-      console.log(state);
 
       return {
         ...state,
         items: {...state.items, [addedProduct.id]: newOrExistingItem},
         totalAmount: state.totalAmount + prodPrice
+      };
+
+    case REMOVE_FROM_CART:
+      const selectedCartItem = state.items[action.pid];
+      const currentQuantity = selectedCartItem.quantity;
+      let updatedCartItems;
+
+      if (currentQuantity > 1){
+        // need to reduce it, not erase
+        const updatedCartItem = new CartItem(
+          selectedCartItem.quantity - 1,
+          selectedCartItem.productPrice,
+          selectedCartItem.productTitle,
+          selectedCartItem.sum - selectedCartItem.productPrice
+        );
+        updatedCartItems = {...state.items, [action.pid]: updatedCartItem}
+      } else {
+        //erase
+        updatedCartItems = {...state.items};
+        delete updatedCartItems[action.pid];
+      };
+
+      console.log("total amount: " + state.totalAmount);
+      console.log("price: " + selectedCartItem.productPrice);
+
+      return {
+        ...state,
+        items: updatedCartItems,
+        totalAmount: state.totalAmount - selectedCartItem.productPrice
       }
   }
 

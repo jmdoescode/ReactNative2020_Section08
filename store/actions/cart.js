698dbd036500081c6b1310e889ad194d39d5c2fc
JM De Ocampo <jmdoescode@gmail.com>
2020-10-19 22:17:02 -0700

    formatting - fixed formatting

--- store/actions/cart.js
+++ store/actions/cart.js
@@ -6,5 +6,8 @@ export const addToCart = product => {
 }
 
 export const removeFromCart = productId => {
-  return {type: REMOVE_FROM_CART, pid: productId}
+  return {
+    type: REMOVE_FROM_CART,
+    pid: productId
+  }
 }

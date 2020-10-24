f5fcd25cc379bcac4547d1656105c6fe161f8f11
JM De Ocampo <jmdoescode@gmail.com>
2020-10-23 23:59:45 -0700

    10.207 - Storing orders

--- store/actions/order.js
+++ store/actions/order.js
@@ -1,11 +1,30 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => {
-  return {
-    type: ADD_ORDER,
-    orderData: {
-      items: cartItems,
-      amount: totalAmount
-    }
+  return async dispatch => {
+    const date = new Date();
+    const response = await fetch('https://rn-complete-guide-dc18b.firebaseio.com/orders/u1.json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        cartItems,
+        totalAmount,
+        date: date.toISOString()
+      })
+    });
+
+    const responseData = await response.json();
+
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: responseData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date
+      }
+    })
   }
 }

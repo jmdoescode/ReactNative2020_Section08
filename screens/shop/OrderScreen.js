575b021d972f45a806043cfbe22a29a8fcdd4497
JM De Ocampo <jmdoescode@gmail.com>
2020-10-25 10:56:58 -0700

    10.209 - Fetching Stored Orders

--- screens/shop/OrderScreen.js
+++ screens/shop/OrderScreen.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import {Text, FlatList, Platform} from 'react-native';
-import {useSelector} from 'react-redux';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList, Platform, ActivityIndicator, StyleSheet} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
 import {HeaderButtons, Item} from "react-navigation-header-buttons";
 import CustomHeaderButton from "../../components/UI/HeaderButton";
 import OrderItem from "../../components/shop/OrderItem";
+import * as ordersActions from './../../store/actions/order'
+import Colors from "../../constants/Colors";
 
 const OrderScreen = props => {
+  const [isLoading, setIsLoading] = useState(false);
   const orders = useSelector(state => state.orders.orders);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setIsLoading(true);
+    dispatch(ordersActions.fetchOrders()).then(() => {
+      setIsLoading(false);
+    });
+  }, [dispatch]);
+
+  if(isLoading){
+    return <View>
+      <ActivityIndicator size='large' Color={Colors.primary} style={styles.centered} />
+    </View>
+  }
 
   return (
     <FlatList
@@ -34,5 +51,13 @@ OrderScreen.navigationOptions = navData => {
     </HeaderButtons>
   }
 };
+
+const styles = StyleSheet.create({
+  centered: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default OrderScreen;
--- store/actions/order.js
+++ store/actions/order.js
@@ -1,4 +1,39 @@
+import Product from "../../models/product";
+import {SET_PRODUCTS} from "./product";
+import Order from "../../models/order";
+
 export const ADD_ORDER = "ADD_ORDER";
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch('https://rn-complete-guide-dc18b.firebaseio.com/orders/u1.json');
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+      const responseData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in responseData) {
+        loadedOrders.push(new Order(
+          key,
+          responseData[key].items,
+          responseData[key].totalAmount,
+          new Date(responseData[key].date) //10.209 - Need date object not just the string
+        ))
+      }
+
+      dispatch({
+        type: SET_ORDERS,
+        orders: loadedOrders
+      })
+    } catch (err) {
+      //TODO: send to analytics server
+      throw err;
+    }
+  }
+}
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {

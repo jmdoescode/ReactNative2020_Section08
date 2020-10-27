e8d072cd913fe34ae2e556d23cc97704678584b6
JM De Ocampo <jmdoescode@gmail.com>
2020-10-26 22:47:15 -0700

    11.224 - Implementing Auto Logout

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ import {Provider} from 'react-redux';
 import ReduxThunk from 'redux-thunk';
 
 import productsReducer from './store/reducers/products';
-import ShopNavigator from './navigation/ShopNavigator';
 import cartReducer from './store/reducers/cart';
 import ordersReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
+import NavigationContainer from "./navigation/NavigationContainer";
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -46,7 +46,7 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <ShopNavigator />
+      <NavigationContainer />
     </Provider>
   );
 }
